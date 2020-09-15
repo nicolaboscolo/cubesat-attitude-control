500cb0b546617e0301020421e06b15985d3a66c4
nicolaboscolo <57726347+nicolaboscolo@users.noreply.github.com>
2020-09-15 13:29:06 +0200

    Update spher2car.m

--- funs/spher2car.m
+++ funs/spher2car.m
@@ -3,6 +3,6 @@ function X = spher2car(sph_coord)
 %   INPUT [Right accencion [rad], declination [rad], radius [whatever]]
 %   OUTPUT [X Y Z]
 RA=sph_coord(:,1);decl=sph_coord(:,2);r=sph_coord(:,3);
-X = r.*[cos(decl).*cos(RA) cos(decl).*sin(RA) sin(decl)]';
+X = (r.*[cos(decl).*cos(RA) cos(decl).*sin(RA) sin(decl)])';
 end
 
